db2ef9dfd4e7d79f7412491af4a63148226316a6
Greg Orvik <gvorvik@gmail.com>
2018-04-17 11:07:14 -0500

    add express

---
+++ script.js
@@ -0,0 +1,10 @@
+//node is just a runtime environment for server-side javascript
+//express is a library of server side javascript (like jquery is for client-side)
+
+//npm (node package manager) is tool we use to manage packages and libraries
+    //npm init within our project sets up npm and puts in package.json file
+//A server is something that is continuosly running, and when it gets a request it will send a response (req, res)
+
+//When we download node modules, we don't want to commit all of them up to github
+    //use .gitignore to place the modules you don't want 
+    //DO NOT IGNORE THE GIT IGNORE (push that to github)
