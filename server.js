d525c7dc4418b3c556ec054b3559b2191fc37486
Greg Orvik <gvorvik@gmail.com>
2018-04-17 11:52:21 -0500

    add git requests

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Bring express into the project
-    //This is saying 'got to the node module and find something called express'
-    //Whatever express exports, that is what we are importing by using require
+//This is saying 'got to the node module and find something called express'
+//Whatever express exports, that is what we are importing by using require
 const express = require('express');
 
 //This will be an instance of express
@@ -8,4 +8,15 @@ const app = express();
 //This is a location
 const PORT = 5000;
 
-app.listen(PORT);
+//a get request for local host (req = request, res = response)
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.get('/hey', (req, res) => {
+    res.send('Howdy');
+});
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`);
+});
